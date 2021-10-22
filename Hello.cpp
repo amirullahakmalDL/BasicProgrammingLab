a524670f87db1e4c59d5488bec730c3dd1f8b7c6
Amirullah Akmal <92998348+ayayadeeznut@users.noreply.github.com>
2021-10-22 23:31:22 +0800

    Create Hello.cpp
    This is introductory file for this lab. Feel free to look for my file here

---
+++ Hello.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  
+  cout<<"Hello Malaysia"<<endl;
+  cout<<"My name is Amir"<<endl;
+  cout<<"I am your lab demonstrator for Basic Programming"<<endl;
+  cout<<"I am currently doing master program in Computing in IIUM"<<endl;
+  cout<<"Introduce yourself : (Your name, matric no, Place of Origins) "<<endl;
+  
+  return 0;
+}
