0463323bc273759f65ea656e5018d2c8c5af6204
Amirullah Akmal <92998348+ayayadeeznut@users.noreply.github.com>
2021-10-31 21:28:26 +0800

    Add files via upload

---
+++ Intro.cpp
@@ -0,0 +1,19 @@
+#include<iostream> 
+#include<string>
+using namespace std; 
+
+
+int main() 
+{
+  // Now we are going to display the variable
+  string name = "Amir";
+  int matric_no = 12345;
+  string place_of_origins = "Miri, Sarawak";
+  cout<<"Hello Malaysia"<<endl; 
+  cout<<"Introduce yourself : "<<endl;
+  cout<<"Name: "<<name<<endl;
+  cout<<"Matric No: "<<matric_no<<endl;
+  cout<<"Place of Origins: "<<place_of_origins<<endl;
+  
+  return 0; 
+}
