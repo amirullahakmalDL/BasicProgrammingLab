988e5b0e67929bada8e6a7000a8643f641a465dc
Amirullah Akmal <92998348+ayayadeeznut@users.noreply.github.com>
2021-11-01 21:14:16 +0800

    Add files via upload

---
+++ Lab1-Ex1.cpp
@@ -0,0 +1,26 @@
+
+
+#include<iostream> 
+using namespace std; 
+int main()
+{
+int N = 12; //int = for number
+int a = 1;
+float f = 2.76; //float = for big number 
+double g = 3.141592653589793; //double =for bigger number 
+string s = "Ikan"; //string = for sentences
+char A = a; //char = for word (use char[number] for longer words)
+bool b = false; //boilean logic = for logics
+double x, y, z; //not necessary to declare value of variable on variable declaration
+
+x = 1.0;
+y = 2.0;
+z = 4.0;
+
+
+
+cout << "This is our first C++ Program."<<endl;
+cout << "It Works!"<<endl;
+return 0;
+}
+
---
+++ Lab1-Ex2.cpp
@@ -0,0 +1,11 @@
+#include<iostream> 
+using namespace std; 
+int main( )
+{
+cout<<"C++ Primer is a highly recommended, well organised and well-written book \n\n"<<endl;
+cout<<"for those who want to learn C++. \n\n"<<endl;
+cout<<"It is a very thorough introduction to the language in a very accessible format \n\n"<<endl;
+cout<<"with plenty of examples and explains everything in the language in great detail. \n\n"<<endl;
+
+return 0;
+}
