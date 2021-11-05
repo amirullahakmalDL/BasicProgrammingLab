cf8e4278489258a9084fd8e23dbcddbbed7107a3
Amirullah Akmal <92998348+ayayadeeznut@users.noreply.github.com>
2021-11-05 09:54:59 +0800

    Add files via upload

---
+++ Lab1-Ex3.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+
+    cout<<"************************"<<endl;
+    cout<<"*                      *"<<endl;
+    cout<<"*                      *"<<endl;
+    cout<<"*                      *"<<endl;
+    cout<<"*                      *"<<endl;
+    cout<<"*                      *"<<endl;
+    cout<<"*                      *"<<endl;
+    cout<<"************************ \n"<<endl;
+
+    cout<<"*****************"<<endl;
+    cout<<"*\t\t*"<<endl;
+    cout<<"*\t\t*"<<endl;
+    cout<<"*\t\t*"<<endl;
+    cout<<"*****************"<<endl;
+
+
+
+
+
+    return 0;
+}
---
+++ Lab1-Ex4.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    cout<<"Hello Malaysia"<<endl;
+    cout<<"              Apa Khabar Malaysia"<<endl;
+    cout<<"                                 Good Morning Malaysia\n"<<endl;
+
+    cout<<"Hello Sarawak"<<endl;
+    cout<<"\t\t\t Apa Khabar Sarawak"<<endl;
+    cout<<"\t\t\t\t\t\t Good Morning Sarawak"<<endl;
+
+    return 0;
+}
